18150580aebd299d4203ee3ef7c65abfeba7f0a9
umershaikh123 <umershaikh217@gmail.com>
2024-10-09 23:24:59 +0500

    fix : About us page fix

--- src/app/(pages)/About.tsx
+++ src/app/(pages)/About.tsx
@@ -8,7 +8,7 @@ import { motion } from "framer-motion"
 
 const About = () => {
   return (
-    <div className="h-[100vh] overflow-x-clip">
+    <div className="h-full overflow-x-clip">
       <Header
         title="About us"
         subtitle="Unlocking the Human Side of the Decentralized Web"
@@ -20,10 +20,10 @@ const About = () => {
           whileInView={{ opacity: 1, x: 0 }}
           transition={{ duration: 1, ease: "easeInOut" }}
           viewport={{ once: true }}
-          className="bg-white px-4 py-24 items-center justify-center flex flex-col min-[900px]:w-1/2 w-full space-y-4"
+          className="bg-white px-4 py-10 items-center justify-center flex flex-col min-[900px]:w-1/2 w-full space-y-4"
           id="section1"
         >
-          <h1 className="text-[var(--primary)] text-7xl  font-semibold">
+          <h1 className="text-[var(--primary)] text-6xl  font-semibold">
             What we do?
           </h1>
           <p className="text-[var(--background)] font-semibold text-2xl px-16 text-center">
@@ -39,10 +39,10 @@ const About = () => {
             whileInView={{ opacity: 1, x: 0 }}
             transition={{ duration: 1, ease: "easeInOut" }}
             viewport={{ once: true }}
-            className="bg-[var(--primary)] text-[var(--background)] px-4 py-24 flex flex-col items-center justify-center space-y-4"
+            className="bg-[var(--primary)] text-[var(--background)] px-4 py-10 flex flex-col items-center justify-center space-y-4"
             id="section2"
           >
-            <h1 className="text-7xl  font-semibold">Our Mission</h1>
+            <h1 className="text-6xl  font-semibold">Our Mission</h1>
             <p className="font-semibold text-2xl px-16 text-center">
               To reveal the real-world impact and opportunities of Web3 for
               everyone, regardless of technical expertise.
@@ -54,10 +54,10 @@ const About = () => {
             whileInView={{ opacity: 1, x: 0 }}
             transition={{ duration: 1, ease: "easeInOut" }}
             viewport={{ once: true }}
-            className="bg-[var(--background)] text-white px-4 py-24 flex flex-col items-center justify-center space-y-4"
+            className="bg-[var(--background)] text-white px-4 py-10 flex flex-col items-center justify-center space-y-4"
             id="section3"
           >
-            <h1 className="text-7xl  font-semibold">Join Us</h1>
+            <h1 className="text-6xl  font-semibold">Join Us</h1>
             <p className="font-semibold text-2xl px-16 text-center">
               To discover how Web3 could create new opportunities and reshape
               industries – no coding skills required.
